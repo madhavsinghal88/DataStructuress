8d7032f2efabf5ba74934cfea07d72ef46be1c2c
Madhav Singhal <madhavsinghal48@gmail.com>
2025-10-16 11:35:44 +0530

    largest and binary

--- src/LargestNumSearch.java
+++ src/LargestNumSearch.java
@@ -3,18 +3,24 @@ import java.util.*;
 public class LargestNumSearch {
     public static int getLargestNumSearch(int[] numbers){
         int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
 
         for(int i = 0; i<numbers.length; i++){
             if(largest < numbers[i]){
                 largest = numbers[i];
 
             }
+            if(smallest > numbers[i]){
+                    smallest = numbers[i];
+            }
         }
+//        return smallest;
+        System.out.println("smallest value is : " + smallest);
         return largest;
     }
     public static void main(String[] args){
         int[] numbers = {3,64,78,23,12,97};
-        System.out.println("largest value is :" + getLargestNumSearch(numbers));
+        System.out.println("largest value is : " + getLargestNumSearch(numbers));
     }
 
 
---
+++ src/binarySearch.java
@@ -0,0 +1,30 @@
+public class binarySearch {
+
+    public static int binaryS(int[] binnum,int target){
+        int start = 0;
+        int end = binnum.length-1;
+
+        while( start<=end){
+
+            int mid = (start+end)/2;
+                    if (binnum[mid]==target){
+                        return mid;
+        }
+                    if(binnum[mid]<target){
+                        start=mid+1;
+
+                    } else {
+                        end = mid -1 ;
+                    }
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] binnum = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        int target = 3;
+
+        System.out.println("index for key is :" + binaryS(binnum,target));
+    }
+}
